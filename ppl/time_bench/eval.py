da2716321c971c46ba3950e01c8e4429868e9ef1
zhichao-aws <zhichaog@amazon.com>
2025-05-06 13:51:06 +0800

    time bench
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>

---
+++ ppl/time_bench/eval.py
@@ -0,0 +1,26 @@
+import json
+import argparse
+
+from utils import invoke, evaluate_time_predictions
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--version", type=str, default="v0", help="Version number of prompts"
+)
+args = parser.parse_args()
+
+with open(f"prompts/{args.version}.txt") as f:
+    prompt = f.read()
+
+with open("time_bench.json") as f:
+    time_bench = json.load(f)
+
+predicts = []
+for example in tqdm(time_bench):
+    predicts.append(invoke(example["question"], example["now"], prompt))
+
+eval_res = evaluate_time_predictions(time_bench, predicts, True)
+
+with open(f"prompts/{args.version}_res.json", "w") as f:
+    json.dump(eval_res, f, indent=4)
