4fabf01875a4b1dd45d5a8c755e2ce90093e7dde
zhichao-aws <zhichaog@amazon.com>
2025-05-23 06:55:35 +0000

    read access key and secret key
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>

--- ppl/time_bench/utils.py
+++ ppl/time_bench/utils.py
@@ -10,6 +10,8 @@ from typing import Dict, Any, Optional
 def setup_bedrock():
     session = boto3.Session(
         region_name=os.environ.get("AWS_REGION", "us-east-1"),
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY", None),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY", None),
     )
     return session.client("bedrock-runtime")
 
